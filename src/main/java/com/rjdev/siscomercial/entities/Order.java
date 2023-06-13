849db8764352bbd098e121e6ba70fcb59b4a47db
RilleJMattos <rille21@gmail.com>
2023-06-13 09:45:17 -0300

    Payment

--- src/main/java/com/rjdev/siscomercial/entities/Order.java
+++ src/main/java/com/rjdev/siscomercial/entities/Order.java
@@ -1,6 +1,7 @@
 package com.rjdev.siscomercial.entities;
 
 import jakarta.persistence.*;
+import java.time.Instant;
 
 @Entity
 @Table(name = "tb_oder")
@@ -11,10 +12,64 @@ public class Order {
     private Long id;
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-    private Integer moment;
+    private Instant moment;
     private OrderStatus status;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public Order(){
+    }
+
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
+        this.id = id;
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+        this.payment = payment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 }
--- src/main/java/com/rjdev/siscomercial/entities/OrderStatus.java
+++ src/main/java/com/rjdev/siscomercial/entities/OrderStatus.java
@@ -1,5 +1,5 @@
 package com.rjdev.siscomercial.entities;
 
 public enum OrderStatus {
-    WAITING_PAYMENT , PAID, SHIPPED, DELIVERED, CANCELED;
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
 }
